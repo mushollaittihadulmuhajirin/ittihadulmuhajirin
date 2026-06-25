2fddbecd1a9f57afc2456e4a95fc76f56c547394
mushollaittihadulmuhajirin <mushollaittihadulmuhajirin@gmail.com>
2026-06-25 10:29:52 +0700

    fix mysql pool for vercel

--- config/db.js
+++ config/db.js
@@ -1,20 +1,26 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-db.connect((err)=>{
-    if(err){
+db.getConnection((err, connection) => {
+
+    if (err) {
         console.log("Koneksi database gagal");
         console.log(err);
-    }else{
+    } else {
         console.log("MySQL Connected");
+        connection.release();
     }
+
 });
 
 module.exports = db;
